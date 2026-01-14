374475d2c03e75429300c925537439b0efa2a8b1
Shwetha Kulal <90205229+shwethajk@users.noreply.github.com>
2026-01-14 17:00:11 +0530

    Add files via upload

---
+++ UI_Tests/src/test/java/pages/LoginPage.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    private WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void login(String username, String password) {
+        driver.findElement(By.id("username")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("loginBtn")).click();
+    }
+}
