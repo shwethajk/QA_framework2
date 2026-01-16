354a8898c0964f78aa6300e2c4b19982ffd3459b
Shwetha Kulal <90205229+shwethajk@users.noreply.github.com>
2026-01-16 14:45:30 +0530

    Refactor login test to validate successful login

--- UI_Tests/src/test/java/tests/LoginTest.java
+++ UI_Tests/src/test/java/tests/LoginTest.java
@@ -1,16 +1,39 @@
 package tests;
 
 import base.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import pages.LoginPage;
+
+import java.time.Duration;
 
 public class LoginTest extends BaseTest {
 
     @Test
-    public void sampleTest() {
-        LoginPage login = new LoginPage(driver);
-        login.login("user", "pass");
-        Assert.assertTrue(driver.getTitle() != null);
+    public void validLoginTest() {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        WebElement username = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.id("username"))
+        );
+        username.sendKeys("tomsmith");
+
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("SuperSecretPassword!");
+
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        WebElement successMsg = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.id("flash"))
+        );
+
+        Assert.assertTrue(
+                successMsg.getText().contains("You logged into a secure area"),
+                "Login failed!"
+        );
     }
 }
