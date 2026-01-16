930052e411532be2dec514c7e4bf29ace12921ba
Shwetha Kulal <90205229+shwethajk@users.noreply.github.com>
2026-01-16 14:12:43 +0530

    Configure ChromeDriver with headless options
    Added ChromeOptions for headless browser testing.

--- UI_Tests/src/test/java/base/BaseTest.java
+++ UI_Tests/src/test/java/base/BaseTest.java
@@ -1,5 +1,16 @@
 package base;
 
+ChromeOptions options = new ChromeOptions();
+
+options.addArguments("--headless=new"); // REQUIRED
+options.addArguments("--no-sandbox"); // REQUIRED for GitHub runners
+options.addArguments("--disable-dev-shm-usage"); // REQUIRED
+options.addArguments("--disable-gpu"); // Safe
+options.addArguments("--window-size=1920,1080");
+
+WebDriver driver = new ChromeDriver(options);
+
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
