6e1d30bcfacddba694ef908e1659465351067f72
Shwetha Kulal <90205229+shwethajk@users.noreply.github.com>
2026-01-17 01:53:24 +0530

    Update run_tests.sh

--- Unit_tests_gtest/scripts/run_tests.sh
+++ Unit_tests_gtest/scripts/run_tests.sh
@@ -13,14 +13,14 @@ echo "Cleaning old build and reports..."
 rm -rf $BUILD_DIR
 rm -rf reports
 
-echo "Creating build directory..."
+echo "Creating build and report directories..."
 mkdir -p $BUILD_DIR
 mkdir -p $REPORT_DIR
 
 cd $BUILD_DIR
 
 echo "Configuring CMake..."
-cmake ..
+cmake .. -DCMAKE_CXX_FLAGS="--coverage -O0 -g" -DCMAKE_C_FLAGS="--coverage -O0 -g"
 
 echo "Building..."
 make -j$(nproc)
@@ -29,14 +29,12 @@ echo "Running tests..."
 ctest --output-on-failure
 
 echo "Generating coverage report..."
+cd $ROOT_DIR/$REPORT_DIR   # move to the report folder
+gcovr -r $ROOT_DIR --xml-pretty -o coverage.xml -v
 
-gcovr \
-  --root .. \
-  --xml-pretty \
-  --output ../$REPORT_DIR/coverage.xml
-ls -l reports/gtest
-
-gcovr -r .. --xml-pretty -o ../reports/gtest/coverage.xml -v
-
-
-echo "Reports generated at: $REPORT_DIR"
+if [ -f coverage.xml ]; then
+    echo "Coverage report successfully created at $(pwd)/coverage.xml"
+else
+    echo "Error: coverage.xml not generated!"
+    exit 1
+fi
