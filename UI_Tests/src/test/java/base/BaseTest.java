78fea3dae1891da2abbba61bb7f56c3e2684f18b
Shwetha Kulal <90205229+shwethajk@users.noreply.github.com>
2026-01-16 14:36:44 +0530

    Configure ChromeDriver options for CI environment

--- UI_Tests/src/test/java/base/BaseTest.java
+++ UI_Tests/src/test/java/base/BaseTest.java
@@ -1,32 +1,34 @@
 package base;
-if (System.getenv("CI") != null) {
-    options.addArguments("--headless=new");
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage")
-    options.addArguments("--disable-gpu"); // Safe
-    options.addArguments("--window-size=1920,1080");
-}
-
-ChromeOptions options = new ChromeOptions();
-
-WebDriver driver = new ChromeDriver(options);
-
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
+
     protected WebDriver driver;
 
     @BeforeMethod
     public void setup() {
+
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+
+        if (System.getenv("CI") != null) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        driver.get("https://example.com"); // demo URL
+        driver.get("https://example.com");
     }
 
     @AfterMethod
